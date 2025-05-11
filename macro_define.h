fd4850cb425e3b574013c52a609cce61b3eef8bb
tyh1003 <elaine921003@gmail.com>
2025-05-11 13:59:16 +0800

    更新 README

--- macro_define.h
+++ macro_define.h
@@ -6,24 +6,23 @@
 #define macro_calc_fft_ratio fft_ratio = (add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI +  others_cnt * others_CPI\
     + fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI)/fft_cycle_count;
 
-#define macro_arraymul_vector_cycle_count arraymul_vector_cycle_count = add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI\
-    + fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI + flw_cnt * flw_CPI + fsw_cnt * fsw_CPI ;
+#define macro_arraymul_vector_cycle_count arraymul_vector_cycle_count = add_cnt * add_CPI + sub_cnt * sub_CPI + lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI\
+    + fadd_cnt * fadd_CPI  + fmul_cnt * fmul_CPI ;
 
 #define macro_arraymul_vector_cpu_time arraymul_vector_cpu_time = arraymul_vector_cycle_count * cycle_time ;
 
-#define macro_arraymul_baseline_cycle_count arraymul_baseline_cycle_count = add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI\
-    + fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI + flw_cnt * flw_CPI + fsw_cnt * fsw_CPI ;
+#define macro_arraymul_baseline_cycle_count arraymul_baseline_cycle_count = add_cnt * add_CPI + others_cnt * others_CPI\
+    + fadd_cnt * fadd_CPI + fmul_cnt * fmul_CPI + flw_cnt * flw_CPI + fsw_cnt * fsw_CPI ;
 
 #define macro_arraymul_baseline_cpu_time arraymul_baseline_cpu_time = arraymul_baseline_cycle_count * cycle_time ;
 
-#define macro_calc_arraymul_baseline_ratio arraymul_baseline_ratio = (add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + others_cnt * others_CPI+fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI)/arraymul_baseline_cycle_count;
+#define macro_calc_arraymul_baseline_ratio arraymul_baseline_ratio = (add_cnt * add_CPI +  others_cnt * others_CPI+fadd_cnt * fadd_CPI + fmul_cnt * fmul_CPI )/arraymul_baseline_cycle_count;
 
-#define macro_calc_arraymul_double_ratio arraymul_baseline_ratio = (add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + others_cnt * others_CPI+fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI \
-    + dadd_cnt * dadd_CPI + dsub_cnt * dsub_CPI + dmul_cnt * dmul_CPI + ddiv_cnt * ddiv_CPI )/arraymul_double_cycle_count;
+#define macro_calc_arraymul_double_ratio arraymul_baseline_ratio = (add_cnt * add_CPI + others_cnt * others_CPI \
+    + dmul_cnt * dmul_CPI )/arraymul_double_cycle_count;
 
-#define macro_arraymul_double_cycle_count arraymul_double_cycle_count = add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI\
-+ fadd_cnt * fadd_CPI + fsub_cnt * fsub_CPI + fmul_cnt * fmul_CPI + fdiv_cnt * fdiv_CPI + flw_cnt * flw_CPI + fsw_cnt * fsw_CPI \
-+ dadd_cnt * dadd_CPI + dsub_cnt * dsub_CPI + dmul_cnt * dmul_CPI + ddiv_cnt * ddiv_CPI + dlw_cnt * dlw_CPI + dsw_cnt * dsw_CPI ;
+#define macro_arraymul_double_cycle_count arraymul_double_cycle_count = add_cnt * add_CPI + others_cnt * others_CPI\
++  dmul_cnt * dmul_CPI + dlw_cnt * dlw_CPI + dsw_cnt * dsw_CPI ;
 
 #define macro_arraymul_double_cpu_time arraymul_double_cpu_time = arraymul_double_cycle_count * cycle_time;
 
